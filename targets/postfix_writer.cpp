58ca2199f828be70cd85019440a3b8b13f6bf529
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-04-27 11:48:15 +0100

    feat: add indexptr_node

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -302,6 +302,13 @@ void til::postfix_writer::do_nullptr_node(til::nullptr_node * const node, int lv
 
 //---------------------------------------------------------------------------
 
+void til::postfix_writer::do_indexptr_node(til::indexptr_node * const node, int lvl) {
+  // TODO: not needed for now
+  throw "not done yet";
+}
+
+//---------------------------------------------------------------------------
+
 void til::postfix_writer::do_if_node(til::if_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
   int lbl1;
--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -227,6 +227,14 @@ void til::type_checker::do_nullptr_node(til::nullptr_node *const node, int lvl) 
 
 //---------------------------------------------------------------------------
 
+void til::type_checker::do_indexptr_node(til::indexptr_node *const node, int lvl) {
+  // TODO: not needed for now
+  throw "not done yet";
+}
+
+
+//---------------------------------------------------------------------------
+
 void til::type_checker::do_function_call_node(til::function_call_node *const node, int lvl) {
   // TODO: implement this
   throw "not implemented";
