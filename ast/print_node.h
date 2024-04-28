a0f5110dc579e588ba7c465a3e9e0394b7bc02ff
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-04-28 14:03:52 +0100

    feat: add println node

--- ast/print_node.h
+++ ast/print_node.h
@@ -10,10 +10,11 @@ namespace til {
    */
   class print_node : public cdk::basic_node {
     cdk::expression_node *_argument;
+    bool _newline;
 
   public:
-    print_node(int lineno, cdk::expression_node *argument) :
-        cdk::basic_node(lineno), _argument(argument) {
+    print_node(int lineno, cdk::expression_node *argument, bool newline = false) :
+        cdk::basic_node(lineno), _argument(argument), _newline(newline) {
     }
 
     cdk::expression_node *argument() { return _argument; }
