cdf923b9fff3c51d3851188f87c8ca5db1abb2aa
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-05-04 15:54:11 +0100

    fix: change attribute type on print_node

--- ast/print_node.h
+++ ast/print_node.h
@@ -1,7 +1,7 @@
 #ifndef __SIMPLE_AST_PRINT_NODE_H__
 #define __SIMPLE_AST_PRINT_NODE_H__
 
-#include <cdk/ast/expression_node.h>
+#include <cdk/ast/sequence_node.h>
 
 namespace til {
 
@@ -9,15 +9,15 @@ namespace til {
    * Class for describing print nodes.
    */
   class print_node : public cdk::basic_node {
-    cdk::expression_node *_argument;
+    cdk::sequence_node *_arguments;
     bool _newline;
 
   public:
-    print_node(int lineno, cdk::expression_node *argument, bool newline = false) :
-        cdk::basic_node(lineno), _argument(argument), _newline(newline) {
+    print_node(int lineno, cdk::sequence_node *arguments, bool newline = false) :
+        cdk::basic_node(lineno), _arguments(arguments), _newline(newline) {
     }
 
-    cdk::expression_node *argument() { return _argument; }
+    cdk::sequence_node *arguments() { return _arguments; }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_print_node(this, level); }
 
