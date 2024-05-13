71f16230b13ca8479ab9d782094c6527da16c773
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-05-13 11:42:19 +0100

    feat: add xml_writer

--- targets/xml_writer.h
+++ targets/xml_writer.h
@@ -3,6 +3,9 @@
 
 #include "targets/basic_ast_visitor.h"
 #include <cdk/ast/basic_node.h>
+#include <cdk/types/types.h>
+
+#include "til_parser.tab.h"
 
 namespace til {
 
@@ -29,12 +32,6 @@ namespace til {
     void openTag(const cdk::basic_node *node, int lvl) {
       openTag(node->label(), lvl);
     }
-    void closeTag(const std::string &tag, int lvl) {
-      os() << std::string(lvl, ' ') + "</" + tag + ">" << std::endl;
-    }
-    void closeTag(const cdk::basic_node *node, int lvl) {
-      closeTag(node->label(), lvl);
-    }
     template<class... Attributes>
     void openTagWithAttributes(const std::string &tag, int lvl, bool empty, Attributes&&... attrs) {
       os() << std::string(lvl, ' ') + "<" + tag;
@@ -43,7 +40,10 @@ namespace til {
 
       os() << (empty ? " />" : ">") << std::endl;
     }
-
+    /*
+     * Allow passing attributes to the opening tag.
+     * Example: openTagWithAttributes("tagname", lvl, std::make_pair("key1", "value1"), std::make_pair("key2", "value2"))
+     */
     template<class... Attributes>
     void openTagWithAttributes(const cdk::basic_node *node, int lvl, Attributes&&... attrs) {
       openTagWithAttributes(node->label(), lvl, false, attrs...);
@@ -52,20 +52,68 @@ namespace til {
     void emptyTagWithAttributes(const cdk::basic_node *node, int lvl, Attributes&&... attrs) {
       openTagWithAttributes(node->label(), lvl, true, attrs...);
     }
+    void closeTag(const std::string &tag, int lvl) {
+      os() << std::string(lvl, ' ') + "</" + tag + ">" << std::endl;
+    }
+    void closeTag(const cdk::basic_node *node, int lvl) {
+      closeTag(node->label(), lvl);
+    }
+    template<typename T>
+    void inlineTag(const std::string &tag, int lvl, T &content) {
+      os() << std::string(lvl, ' ') << "<" << tag << ">" << content << "</" << tag << ">" << std::endl;
+    }
+    template<typename T>
+    void inlineTag(const cdk::basic_node *node, int lvl, T &content) {
+      inlineTag(node->label(), lvl, content);
+    }
+    void emptyTag(const std::string &tag, int lvl) {
+      os() << std::string(lvl, ' ') + "<" + tag + " />" << std::endl;
+    }
+    void emptyTag(const cdk::basic_node *node, int lvl) {
+      emptyTag(node->label(), lvl);
+    }
 
   protected:
     void do_binary_operation(cdk::binary_operation_node *const node, int lvl);
     void do_unary_operation(cdk::unary_operation_node *const node, int lvl);
-    template<typename T>
-    void process_literal(cdk::literal_node<T> *const node, int lvl) {
-      os() << std::string(lvl, ' ') << "<" << node->label() << ">" << node->value() << "</" << node->label() << ">" << std::endl;
-    }
-
-    // Auxiliary Functions
     inline const char *bool_to_str(bool boolean) {
       return boolean ? "true" : "false";
     }
-    
+    inline std::string type_to_str(std::shared_ptr<cdk::basic_type> type) {
+      if (type == nullptr) {
+        return "[unknown]"; // why the brackets?
+      }
+
+      auto ftype = std::dynamic_pointer_cast<cdk::functional_type>(type);
+      if (ftype != nullptr) {
+        return "(" + type_to_str(ftype->input()) + ") returns (" + type_to_str(ftype->output()) + ")";
+      }
+
+      auto stype = std::dynamic_pointer_cast<cdk::structured_type>(type);
+      if (stype != nullptr) {
+        auto result = std::string();
+        auto components = stype->components();
+        return std::accumulate(components.begin(), components.end(), result,
+            [this] (auto a, auto b) {
+              return a + (a.empty() ? "" : ", ") + type_to_str(b);
+            });
+      }
+
+      return cdk::to_string(type);
+    }
+    inline const char *qualifier_to_str(int qualifier) {
+      switch (qualifier) {
+        case tEXTERNAL: return "external";
+        case tFORWARD: return "forward";
+        case tPUBLIC: return "public";
+        case tPRIVATE: return "private";
+        default: return "[unknown qualifier]";
+      };
+    }
+    template<typename T>
+    void process_literal(cdk::literal_node<T> *const node, int lvl) {
+      inlineTag(node, lvl, node->value());
+    }
 
   public:
     // do not edit these lines
