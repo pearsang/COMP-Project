e49a3f1e96702d81939421b4a804ec39a444fc8b
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-05-30 11:57:29 +0100

    feat(calculator): program node

--- targets/frame_size_calculator.h
+++ targets/frame_size_calculator.h
@@ -1,38 +1,54 @@
-#ifndef __TIL_TARGETS_FRAME_SIZE_CALCULATOR_H__
-#define __TIL_TARGETS_FRAME_SIZE_CALCULATOR_H__
+#ifndef __TIL_TARGET_FRAME_SIZE_CALCULATOR_H__
+#define __TIL_TARGET_FRAME_SIZE_CALCULATOR_H__
 
 #include "targets/basic_ast_visitor.h"
 
+#include <sstream>
+#include <stack>
+
 namespace til {
 
-  class frame_size_calculator: public basic_ast_visitor {
-    cdk::symbol_table<til::symbol> &_symtab;
-    size_t _localsize;
+class frame_size_calculator : public basic_ast_visitor {
+  cdk::symbol_table<til::symbol> &_symtab;
 
-  public:
-    frame_size_calculator(std::shared_ptr<cdk::compiler> compiler, cdk::symbol_table<til::symbol> &symtab) :
-        basic_ast_visitor(compiler), _symtab(symtab), _localsize(0) {
-    }
+  size_t _localsize;
 
-  public:
-    ~frame_size_calculator() {
-      os().flush();
-    }
+public:
+  frame_size_calculator(std::shared_ptr<cdk::compiler> compiler,
+                        cdk::symbol_table<til::symbol> &symtab)
+      : basic_ast_visitor(compiler), _symtab(symtab), _localsize(0) {}
 
-  public:
-    inline size_t localsize() {
-      return _localsize;
-    }
+public:
+  ~frame_size_calculator();
 
-  public:
+public:
+  size_t localsize() const { return _localsize; }
+
+public:
   // do not edit these lines
 #define __IN_VISITOR_HEADER__
-#include ".auto/visitor_decls.h"       // automatically generated
+#include ".auto/visitor_decls.h" // automatically generated
 #undef __IN_VISITOR_HEADER__
   // do not edit these lines: end
+};
 
-  };
+} // namespace til
 
-} // til
+//---------------------------------------------------------------------------
+//     HELPER MACRO FOR TYPE CHECKING
+//---------------------------------------------------------------------------
+
+#define CHECK_TYPES(compiler, symtab, node)                                    \
+  {                                                                            \
+    try {                                                                      \
+      til::type_checker checker(compiler, symtab, this);                       \
+      (node)->accept(&checker, 0);                                             \
+    } catch (const std::string &problem) {                                     \
+      std::cerr << (node)->lineno() << ": " << problem << std::endl;           \
+      return;                                                                  \
+    }                                                                          \
+  }
+
+#define ASSERT_SAFE_EXPRESSIONS CHECK_TYPES(_compiler, _symtab, node)
 
 #endif
